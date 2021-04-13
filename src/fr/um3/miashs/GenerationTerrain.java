de9d536730c142359766defc622a2039537597a5
Gorak <Gorak@LAPTOP-3AK7P2VK>
2021-04-14 00:47:42 +0200

    s

--- src/fr/um3/miashs/GenerationTerrain.java
+++ src/fr/um3/miashs/GenerationTerrain.java
@@ -6,7 +6,7 @@ public class GenerationTerrain {
 	// Mettre les petits élements en premier
 	// Mettre les elements du plus petit au plus grand en terme de nombre: potion: 1, piege:1, joueur: n, mur: n, ....
 	// être sûr que le Joueur peut accéder à la victoire
-	
+	private char [][] plateau;
 	private int horizontale;
 	private int verticale;
 	
@@ -32,18 +32,18 @@ public class GenerationTerrain {
 	
 	
 	public void Element(Tuile Tui, GenerationTerrain G, int x, int y) {
-		Tui.setType(TypeTuile.Potion);
+		Tui.setType(TypeTuile.Mur);
 		
 		
 		G.setHorizontale(y);
 		G.setVerticale(x);
-		
-		x = (int)(Math.random() * ((G.getHorizontale() - 0) + 1));
-		y = (int)(Math.random() * ((G.getVerticale() - 0) + 1));	
-		Tui.setX(x);
-		Tui.setY(y);
-		
-		//Trouver moyen de l'afficher
+		int nb = 0;
+		while (nb < (G.getHorizontale() * G.getVerticale())/2){
+			x = (int)(Math.random() * ((G.getHorizontale() - 0) + 1));
+			y = (int)(Math.random() * ((G.getVerticale() - 0) + 1));
+			plateau[x][y] = '#';
+			nb =+ 1;			
+		}	
 	}
 
 }
